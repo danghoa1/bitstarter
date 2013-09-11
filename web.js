4d780f50f0549843d76dbb5c0307b194135da99b
danghoa1 <danghoaiphi@Phis-MacBook-Air.local>
2013-09-10 17:54:30 -0700

    speedyOrder

--- web.js
+++ web.js
@@ -1,21 +1,27 @@
-var express = require('express');
-var fs = require('fs');
+// Include modules
+var express = require('express');	// eases http request/response management
+var fs = require('fs');				// allows file read/write
 
+// Create a HTTP server
 var app = express(express.logger());
 
 
-// Serve files
+// Serve files to client
 app.configure(function () {
 	app.use('/bootstrap', express.static(__dirname + '/bootstrap'));
   	app.use('/css', express.static(__dirname + '/css'));
   	app.use('/js', express.static(__dirname + '/js'));
+  	app.use('/img', express.static(__dirname + '/img'));
+  	
 });
 
-// Router
+// HTTP request Router (pick the right html)
 app.get('/', function(req,res) {
 	res.sendfile(__dirname + '/html/index.html');
 });
 
+
+// Listen to a port
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening to port " + port);
